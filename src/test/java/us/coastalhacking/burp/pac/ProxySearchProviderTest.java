0b93bd72ba266be28f1ccc2144c1e39c3f9dec0c
Jon Passki <jon@passki.us>
2017-10-04 13:36:35 -0700

    Improved test

--- src/test/java/us/coastalhacking/burp/pac/ProxySearchProviderTest.java
+++ src/test/java/us/coastalhacking/burp/pac/ProxySearchProviderTest.java
@@ -7,6 +7,8 @@ import com.github.markusbernhardt.proxy.search.browser.ie.IEProxySearchStrategy;
 import com.github.markusbernhardt.proxy.search.desktop.DesktopProxySearchStrategy;
 import com.github.markusbernhardt.proxy.search.env.EnvProxySearchStrategy;
 import com.github.markusbernhardt.proxy.search.java.JavaProxySearchStrategy;
+import com.github.markusbernhardt.proxy.util.PlatformUtil;
+import com.github.markusbernhardt.proxy.util.PlatformUtil.Platform;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
 import org.junit.Test;
@@ -19,7 +21,10 @@ public class ProxySearchProviderTest {
         TestUtils.createInjector();
     Provider<ProxySearch> proxySearchProvider = injector.getProvider(ProxySearch.class);
     ProxySearch proxySearch = proxySearchProvider.get();
-    assertTrue(proxySearch.toString().contains(IEProxySearchStrategy.class.getName()));
+    // won't run on travis
+    if (PlatformUtil.getCurrentPlattform() == Platform.WIN) {
+      assertTrue(proxySearch.toString().contains(IEProxySearchStrategy.class.getName()));
+    }
     assertTrue(proxySearch.toString().contains(EnvProxySearchStrategy.class.getName()));
     assertTrue(proxySearch.toString().contains(JavaProxySearchStrategy.class.getName()));
     assertTrue(proxySearch.toString().contains(DesktopProxySearchStrategy.class.getName()));
